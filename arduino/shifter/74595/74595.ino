4d73e1e77e3132e82fe933e9f9309130283cf9ea
Rafael Algara <ralgara@gmail.com>
2019-09-01 01:15:45 -0400

    OE PCM WIP

--- arduino/shifter/74595/74595.ino
+++ arduino/shifter/74595/74595.ino
@@ -2,17 +2,18 @@
 int RCLK_PIN = 8;
 int SRCLK_PIN = 9;
 int SER_PIN = 10;
+int OE_PIN = 13;
 
 void setup() {
   //set pins to output because they are addressed in the main loop
   pinMode(RCLK_PIN, OUTPUT);
   pinMode(SRCLK_PIN, OUTPUT);
   pinMode(SER_PIN, OUTPUT);
+  pinMode(OE_PIN, OUTPUT);
+  digitalWrite(OE_PIN, HIGH);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 }
-
-int IDLE_MS = 100;
 
 void shiftOut(byte value) {
   int i=0;
@@ -30,24 +31,37 @@ void shiftOut(byte value) {
 
 const int BITS = 8;
 const int N = 3;
-const byte PERIODS[N] = {2,3,4};
+const byte PERIODS[N] = {1,3,4};
 
-int t = 0;
+int t = 0;  // Object clock: argument to oscillators
+int tz = 0; // Hardware clock: CPU loop counter, basis for PCM intensity control via register OE
+const int R = 2000;  // Object ratio = CPU loops between register updates
+const float Z = 100; // Inverse duty cycle
+boolean oe = LOW;
+boolean oep = HIGH;
 void loop() {
-      //iterate objects and evolve position
-      byte out = 0;
-      for (int i=0; i < N; i++) { // output bits
-          float s = (BITS/2) * (1 + sin(float(t) / PERIODS[i])); //
-          byte ss = 1 << int(s);
-          out |= ss;
-          Serial.print("t=");Serial.print(t);Serial.print(",i= ");Serial.print(i);
-          Serial.print(",s=");Serial.print(s);Serial.print(",ss=");Serial.print(ss);
-          Serial.print(",out=");Serial.print(out);
-          Serial.println();
-      }
-      shiftOut(out);
-      Serial.print("---");Serial.print(out);Serial.print(" ");Serial.print(out);
-      Serial.println();
-      t += 1;
-      delay(200);
+  if (oep != oe) {
+    oe = oep;
+    digitalWrite(OE_PIN, oe);
+    //Serial.print("\noep=");Serial.print(oep);Serial.print("\n");    
+  }
+  oep = sin(tz/Z) > 0;
+  
+  //iterate objects and evolve position
+  if (tz % R == 0) {
+    byte out = 0;
+    for (int i=0; i < N; i++) { // output bits
+      float s = (BITS/2) * (1 + sin(float(t) / PERIODS[i])); //
+      byte ss = 1 << int(s);
+      out |= ss;
+    }
+    shiftOut(out);
+//    Serial.print("tz=");Serial.print(tz);
+//    Serial.print(",t=");Serial.print(t);
+//    Serial.print(",oe=");Serial.print(oe);
+//    Serial.print(",oep=");Serial.print(oep);
+//    Serial.println();
+    t++;
+  }
+  tz++;
 }
